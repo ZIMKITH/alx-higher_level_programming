02ad4abd1f6414a2d029c6c27be490ff4e876ea9
ZIMKITH <ntshikanisozimkitha@gmail.com>
2023-11-28 12:59:05 -0800

    python

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,10 +2,14 @@
 # 6-print_comb3.py
 
 """Prints all possible different combinations of two digits"""
+n = 0
 for i in range(10):
-    for j in range(i + 1, 10):
-        print(f"{i}{j}", end="")
+    j = 1 + n
+    while j < 10:
+        print("{}{}".format(i, j), end="")
         if i == 8 and j == 9:
             break
-        print(", ", end="")
+        j += 1
+        print(",", end=" ")
+    n += 1
 print("")
