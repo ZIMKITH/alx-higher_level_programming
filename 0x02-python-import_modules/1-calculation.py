183d07dce43e620b695d849261f793dffd6c39d8
ZIMKITH <ntshikanisozimkitha@gmail.com>
2023-11-30 10:17:02 -0800

    1-calculation.py

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+
+    sum_result = add(a, b)
+    diff_result = sub(a, b)
+    mul_result = mul(a, b)
+    div_result = div(a, b)
+
+    print("{} + {} = {}".format(a, b, sum_result))
+    print("{} - {} = {}".format(a, b, diff_result))
+    print("{} * {} = {}".format(a, b, mul_result))
+    print("{} / {} = {}".format(a, b, div_result))
