4d33abbae62e561ff9ca3379366368b2cd07ca2d
ZIMKITH <ntshikanisozimkitha@gmail.com>
2024-01-08 12:36:36 -0800

    6-base_geometry.py

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-
-"""
-Geometry Module
-"""
+"""Empty Class BaseGeometry"""
 
 
-class BaseGeometry:
-    """
-    an empty class
-    """
-    pass
+class BaseGeometry():
+    """Constructor of the class"""
+
+    def area(self):
+        """Function to calculate area that is not yet implemented"""
+        raise Exception("area() is not implemented")
